35fedb927e8dbb8037a00be2a793425d45923d2e
abigt <abigt@gmail.com>
2017-04-08 18:35:26 +0800

    Initial Commit

---
+++ FKit/FKit.h
@@ -0,0 +1,19 @@
+//
+//  FKit.h
+//  FKit
+//
+//  Created by 孔祥波 on 08/04/2017.
+//  Copyright © 2017 Kong XiangBo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FKit.
+FOUNDATION_EXPORT double FKitVersionNumber;
+
+//! Project version string for FKit.
+FOUNDATION_EXPORT const unsigned char FKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FKit/PublicHeader.h>
+
+
